d85f613e3d7199e8a9a5ad367787ef7bda73c6a7
Mark Topper <marktopperdk@gmail.com>
2014-12-01 13:49:20 +0100

    Initial Release

---
+++ src/MarkTopper/MobileDetect/MobileDetectServiceProvider.php
@@ -0,0 +1,49 @@
+<?php namespace MarkTopper\MobileDetect;
+
+use Hashids\Hashids;
+use Illuminate\Support\ServiceProvider;
+
+class HashidsServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('mitch/hashids');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+
+	public function register()
+	{
+		$this->registerHashids();
+	}
+
+	protected function registerHashids()
+	{
+		$this->app->bind('Hashids\Hashids', function ($app) {
+			return new Hashids(
+				$app['config']['app.key'],
+				$app['config']['hashids::length'],
+				$app['config']['hashids::alphabet']
+			);
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('Hashids\Hashids');
+	}
+}
